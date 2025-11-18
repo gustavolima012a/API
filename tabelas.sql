795dec35ce232375669b66293cec690e75de64b0
Gustavo Lima <gustavo.lima@eaportal.org>
2025-11-18 20:12:46 -0300

    troca de nome dos arquivos

--- tabelas.sql
+++ tabelas.sql
@@ -1,13 +1,10 @@
-
+CREATE DATABASE IF NOT EXISTS ong_ambiental;
 USE ong_ambiental;
 
-CREATE TABLE ambiental (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome_projeto VARCHAR(100) NOT NULL,
-    tipo_projeto VARCHAR(50) NOT NULL, -- reflorestamento, reciclagem, limpeza, etc.
-    descricao TEXT,
-    data_inicio DATE,
-    data_fim DATE
+CREATE TABLE IF NOT EXISTS vendas_combustivel (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    tipo VARCHAR(50) NOT NULL,
+    litros DECIMAL(10,2) NOT NULL,
+    valor DECIMAL(10,2) NOT NULL,
+    data_venda DATE NOT NULL
 );
-
-SELECT * FROM ambiental;
